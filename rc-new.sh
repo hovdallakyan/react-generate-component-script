5d2cd68f4f1dffccdf2bbad1cc1b16d5b5942ce0
hdallakyan <dallakyanhovhannes@gmail.com>
2019-04-07 21:02:45 +0400

    * Added Hooks for functional components * Added creating scss and test file * Code commenting

--- rc-new.sh
+++ rc-new.sh
@@ -7,24 +7,47 @@ mkdir  $NEW_DIR
 cd $NEW_DIR    
 pwd;
 
+# Creating A SCSS File With The Component's Name
+# Change The .scss To .css Or .less If You Use One Of Those
+touch $1.scss;
+
+# If The Second Argument Is 'C', Create A New Class Based Component
 if [ "$2" = "c" ];
 then
   echo "import React, { Component } from 'react';
+import './$1.scss'
 
 class $1 extends Component {
   render() {
-    return <h1>$1 component</h1>;
+    return (
+      <div>
+      $1 component
+      </div>
+    );
   }
 }
 
-export default $1;" >> index.js;
-else 
-  echo "import React from 'react';
-  
-const $1 = () => (
-  <div>$1 component </div>
-)
+export default $1;" >> ${1}.js;
 
-export default $1;" >> index.js;
+# Creating A New Functional Component
+else 
+  echo "import React, { useState } from 'react';
+import './$1.scss'
+  
+const $1 = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      $1 component 
+    </div>
+  )
+}
+
+export default $1;" >> ${1}.js;
 fi
 
+# Creating File For Testing
+echo "import React from 'react';
+import $1 from './$1';
+import renderer from 'react-test-renderer';" >> ${1}.test.js
